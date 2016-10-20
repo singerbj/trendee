3b75cfb1eafe00272747fcfd59e0a0e138d1cd2e
Ben Singer <benjamin.singer@helpsystems.com>
2016-10-20 15:08:24 -0500

    added other sources

--- server.js
+++ server.js
@@ -23,4 +23,12 @@
 
 
     //https://www.buzzfeed.com/api/v2/feeds/trending
+
+    //http://api.whatthetrend.com/#trends.v2
+
+    //http://www.faroo.com/api?q=&start=1&length=10&l=en&src=topics&f=json
+
+    //https://www.reddit.com/r/all.json
+
+    //https://www.googleapis.com/youtube/v3/videos?part=contentDetails&chart=mostPopular&regionCode=IN&maxResults=25&key=API_KEY //http://stackoverflow.com/questions/30475309/get-youtube-trends-v3-country-wise-in-json
 }());
